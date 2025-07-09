f7c86caeb67140ca0decc89d4c34b21a0e813ca8
alvinvh <58031298+alvinvh@users.noreply.github.com>
2025-07-09 13:57:50 +0800

    change background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import React from 'react';
-import Header from './components/header';
-import Profile from './components/profile';
-import Projects from './components/projects';
-import Skills from './components/skills';
-import Footer from './components/footer';
-import Achievements from './components/achievements';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import React from "react";
+import Header from "./components/header";
+import Profile from "./components/profile";
+import Projects from "./components/projects";
+import Skills from "./components/skills";
+import Footer from "./components/footer";
+import Achievements from "./components/achievements";
+import HexagonParticles from "./components/background";
+import Background from "./components/background";
+
 
 function App() {
   return (
     <div className="App">
+      <Background />
       <Header />
       <Profile />
       <Projects />
@@ -18,7 +22,7 @@ function App() {
       <Skills />
       <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
